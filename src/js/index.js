be6be0782dd75fa150ef482d6fa3be35ac06c115
axe280 <axeworkmail@gmail.com>
2021-03-03 22:30:27 +0200

    support btn, fixes, body overflow

--- src/js/index.js
+++ src/js/index.js
@@ -5,14 +5,25 @@ $(function () {
   svg4everybody()
 
   // page scroll blocker
+  var body = document.body
+  var scrollPosition = 0
+
   var addDocumentScrollBlocker = function () {
-    document.body.classList.add('scroll-page-locked')
-    document.body.style.overflow = 'hidden'
+    body.classList.add('scroll-page-locked')
+    scrollPosition = window.pageYOffset
+    body.style.overflow = 'hidden'
+    body.style.position = 'fixed'
+    body.style.top = `-${scrollPosition}px`
+    body.style.width = '100%'
   }
 
   var removeDocumentScrollBlocker = function () {
-    document.body.classList.remove('scroll-page-locked')
-    document.body.style.removeProperty('overflow')
+    body.classList.remove('scroll-page-locked')
+    body.style.removeProperty('overflow')
+    body.style.removeProperty('position')
+    body.style.removeProperty('top')
+    body.style.removeProperty('width')
+    window.scrollTo(0, scrollPosition)
   }
 
   // company select https://designwithpc.com/Plugins/ddSlick#demo
